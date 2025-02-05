fd2856defba36f936d009d40ad0c557b4fdc1a3b
Sakuraced <chenzs@nuaa.edu.cn>
2025-02-05 22:04:25 +0800

    direct gradient

--- utils/encode.py
+++ utils/encode.py
@@ -60,7 +60,34 @@ def param_to_adj_gumbel(graph, param_mask, param, lora=None):
 
     pred_adj = masked_gumbel_softmax(pred_adj, mask)
     return pred_adj
+def param_to_adj_direct(graph, param_mask, param,lora=None):
+    n = graph.x.size()[0]
+    center_node = graph.center_node
+    edge_index = graph.edge_index
+    device = graph.device
+    mask = torch.zeros((n, n)).to(device)
+    mask[center_node, center_node] = 1
+    mask[edge_index[0, param_mask], edge_index[1, param_mask]] = 1
+    mask[edge_index[1, ~param_mask], edge_index[0, ~param_mask]] = 1
+    pred_adj = torch.zeros((n, n)).to(device)
+    pred_adj[center_node, center_node] = param[0][0]
+    pred_adj[edge_index[0, param_mask], edge_index[1, param_mask]] = param[1][param_mask]
+    pred_adj[edge_index[1, ~param_mask], edge_index[0, ~param_mask]] = param[1][~param_mask]
 
+    if lora!=None:
+        lora=torch.mul(lora, mask)
+        pred_adj+=lora
+    pred_adj = masked_softmax(pred_adj, mask)
+    
+
+    pred_adj = pred_adj.T
+    _, max_indices = pred_adj.max(dim=-1, keepdim=True)
+    y_hard = torch.zeros_like(pred_adj).scatter_(-1, max_indices, 1.0)
+    pred_adj = (y_hard - pred_adj).detach() + pred_adj
+    pred_adj = pred_adj.T
+
+
+    return pred_adj
 def param_to_adj_work(graph, param_mask, param,lora=None):
     n = graph.x.size()[0]
     center_node = graph.center_node
